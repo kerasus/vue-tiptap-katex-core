ffedf21c4233e381c8c8e53348a8e35bfd5cab91
ali <aliesmaeeli20@gmail.com>
2022-06-05 15:20:07 +0430

    - onFileUpload update - version update

--- components/ImageUpload/mixin.js
+++ components/ImageUpload/mixin.js
@@ -10,6 +10,47 @@ const MixinComponentImageUpload = {
         this.setNaturalSize()
     },
     methods: {
+        getValidChainedObject (object, keys) {
+            if (!Array.isArray(keys) && typeof keys !== 'string') {
+                console.warn('keys must be array or string')
+                return false
+            }
+
+            if (keys === '') {
+                return object
+            }
+
+            let keysArray = keys
+            if (typeof keys === 'string') {
+                keysArray = keys.split('.')
+            }
+
+            if (keysArray.length === 1) {
+                if (typeof object === 'undefined' || object === null || typeof object[keysArray[0]] === 'undefined') {
+                    return null
+                }
+                return object[keysArray[0]]
+            }
+
+            if (typeof object[keysArray[0]] !== 'undefined' && object[keysArray[0]] !== null) {
+                return this.getValidChainedObject(object[keysArray[0]], keysArray.splice(1))
+            }
+
+            return (typeof object[keysArray[0]] !== 'undefined' && object[keysArray[0]] !== null)
+        },
+        getImageKeyOfResponse () {
+            const responseKey = this.editor.editorOptions.uploadServer.responseKey
+            if (!responseKey) {
+                return 'url'
+            }
+
+            return responseKey
+        },
+        getImageUrlFromResponse (response) {
+            const responseKey = this.getImageKeyOfResponse()
+
+            return this.getValidChainedObject(response, responseKey)
+        },
         setNaturalSize () {
             if (this.node.attrs.url) {
                 const node = this.node
@@ -28,21 +69,24 @@ const MixinComponentImageUpload = {
             }
         },
         onFileUpload (err, file) {
-            if (!err) {
-                this.updateAttributes({
-                    url: JSON.parse(file.serverId).url
-                })
-                let img = new Image();
-                let that = this
-                img.onload = function() {
-                    that.updateAttributes({
-                        width: img.naturalWidth,
-                        height: img.naturalHeight
-                    })
-                    that.setNaturalSize()
-                };
-                img.src = JSON.parse(file.serverId).url
+            if (err) {
+                return
             }
+
+            const imageUrl = this.getImageUrlFromResponse(JSON.parse(file.serverId))
+            this.updateAttributes({
+                url: imageUrl
+            })
+            let img = new Image();
+            let that = this
+            img.onload = function() {
+                that.updateAttributes({
+                    width: img.naturalWidth,
+                    height: img.naturalHeight
+                })
+                that.setNaturalSize()
+            };
+            img.src = imageUrl
         },
         resizeEnd (event) {
             this.updateAttributes({
